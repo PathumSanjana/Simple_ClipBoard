644a169c3db19b5ce01b39c85e06ec27a8dfdeeb
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-04-13 00:38:07 +0530

    getInstance method added

--- src/com/company/ClipBoard.java
+++ src/com/company/ClipBoard.java
@@ -2,9 +2,21 @@ package com.company;
 
 public class ClipBoard {
 
-
+    //lets try to make this class singleton
     private String content;
+    private static ClipBoard instance;
 
+    //create a private constructor to this class
+    private ClipBoard(){
+    }
+
+    //create a method to get an object of this class
+    public static ClipBoard getInstance(){
+        if(instance == null){
+            instance = new ClipBoard();
+        }
+        return instance;
+    }
 
     //functions in this clipboard class
     public void copy( String newContent){
